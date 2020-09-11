493cc9493c19ea2316ffa7cb2f9e1215b0356bcd
tokiedokie <thetokiedokie@gmail.com>
2020-09-11 10:36:41 +0900

    fix "inferrable types are not allowed"

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@ import { mainDataSrc, versionDataSrc } from "./data_src.ts";
 await writeFiles();
 gitInit();
 
-async function writeFiles(target_path: string = "."): Promise<void> {
+async function writeFiles(target_path = "."): Promise<void> {
   await Deno.mkdir(`${target_path}/src`, { recursive: true });
   Deno.writeTextFile(`${target_path}/src/main.ts`, mainDataSrc);
   Deno.writeTextFile(`${target_path}/version.ts`, versionDataSrc);
 }
 
-async function gitInit(target_path: string = "."): Promise<void> {
+async function gitInit(target_path = "."): Promise<void> {
   Deno.run({
     cmd: ["git", "init"],
-    cwd: target_path
-  })
+    cwd: target_path,
+  });
 }
