e980e6f11b993a7ea9d9670ffb2f562027d7882a
tokiedokie <thetokiedokie@gmail.com>
2020-09-11 17:30:31 +0900

    add --force

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { mainDataSrc, versionDataSrc } from "./data_src.ts";
 main();
 
 async function main() {
-  const target_path = Deno.args[0];
-
-  if (await isDirEmpty(target_path)) {
+  const target_path = Deno.args[0] === "--force" ? Deno.args[1] : Deno.args[0]
+  
+  if (await isDirEmpty(target_path) || Deno.args.includes("--force")) {
     await writeFiles(target_path);
     await gitInit(target_path);
   } else {
-    console.warn("dir is not empty");
+    console.warn("dir is not empty. if you really create deno project in the directory use --force.");
   }
 }
 
